4da48e2d6cb41af6438e6d907853d225cab71f68
Dany Pignoux <dany.pignoux@outlook.fr>
2019-06-08 11:40:15 +0200

    Update erudo.sh

--- screenlayout/erudo.sh
+++ screenlayout/erudo.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate right \
        --output HDMI-1 --primary --mode 1920x1080 --pos 1080x432 --rotate normal \
        --output eDP-1 --mode 1920x1080 --pos 3000x432 --rotate normal \
