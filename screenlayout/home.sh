730779fdded7ef602d089da0c313a17465cc080a
Dany Pignoux <dany.pignoux@outlook.fr>
2019-06-08 11:46:15 +0200

    Add home.sh to screenlayout configuration

---
+++ screenlayout/home.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xrandr --output HDMI-1 --primary --mode 1920x1080 --rotate normal \
+       --output eDP-1 --mode 1920x1080 --rotate normal \
+       --output HDMI-2 --off \
+       --output DP-1 --off \
